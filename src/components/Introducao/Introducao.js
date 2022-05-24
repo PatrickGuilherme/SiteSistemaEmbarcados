1137488064820818986ec89d503547f58e165eef
Patrick Guilherme da Silva <patrick.guilhermedasilva@gmail.com>
2022-05-24 11:28:14 -0300

    init

---
+++ src/components/Introducao/Introducao.js
@@ -0,0 +1,26 @@
+import React from "react";
+import './style.css'
+import imagemExemplo from "../../assets/embarcadoexemplo.png"
+
+function Introducao(){
+    return (
+        <div className="intropage">
+            <div className="texto">
+                <h4>Título</h4>
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make </p>
+                <p>
+                    <a href="#">
+                        <strong>Caracteristicas </strong> 
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+                            <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
+                        </svg>
+                    </a>
+                </p>
+            </div>    
+            <div className="imagemIntro">
+                <img src={imagemExemplo} className="imagemExe"/>
+            </div>
+        </div>
+    );
+}
+export default Introducao;
