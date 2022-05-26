947e2f6d5db5529bb10682c1963990d8ad1e33c1
Patrick Guilherme da Silva <patrick.guilhermedasilva@gmail.com>
2022-05-26 09:45:24 -0300

    Atualização

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import IntroPage from "./components/Titulo/Titulo";
 import Introducao from "./components/Introducao/Introducao"
+import LinhaTempo from './components/LinhaTempo/LinhaTempo';
 
 function App() {
   return (
     <div className='main'>
       <IntroPage/>
       <Introducao/>
+      <LinhaTempo/>
     </div>
   );
 }
--- src/components/Introducao/Introducao.js
+++ src/components/Introducao/Introducao.js
@@ -1,26 +1,105 @@
 import React from "react";
 import './style.css'
 import imagemExemplo from "../../assets/embarcadoexemplo.png"
+import Box from '@mui/material/Box';
+import Modal from '@mui/material/Modal';
+import Button from '@mui/material/Button';
+
+const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+};
 
 function Introducao(){
+    const [open, setOpen] = React.useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
+
     return (
-        <div className="intropage">
-            <div className="texto">
-                <h4>Título</h4>
-                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make </p>
-                <p>
-                    <a href="#">
-                        <strong>Caracteristicas </strong> 
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
-                            <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
-                        </svg>
-                    </a>
-                </p>
-            </div>    
-            <div className="imagemIntro">
-                <img src={imagemExemplo} className="imagemExe"/>
+        <>
+            <div className="intropage">
+                <div className="texto">
+                    <h4>Introdução</h4>
+                    <p>
+                    Diferindo dos computadores que possuem sistemas operacionais e executam softwares dos mais variados os sistemas eletrônicos embarcados são sistemas de processamento de informações que estão embutidos em sistemas maiores e que normalmente não possuem uma interface direta com o usuário (computação invisível), o hardware e software normalmente são integrados e seu projeto visa o desempenho de uma função específica.
+                    </p>
+                    <p>
+                        <a className="linkCaract" onClick={handleOpen}>
+                            <strong>Caracteristicas </strong> 
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+                                <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
+                            </svg>
+                        </a>
+                    </p>
+                </div>    
+                <div className="imagemIntro">
+                    <img src={imagemExemplo} className="imagemExe"/>
+                </div>
             </div>
-        </div>
+            <Modal
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="modal-modal-title"
+                aria-describedby="modal-modal-description"
+                >
+                <div className="modal-dialog modal-dialog-centered" role="document">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h5 className="modal-title" id="exampleModalLongTitle">Caracteristicas</h5>
+                            <button onClick={handleClose} type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div className="modal-body">
+                            <p>
+                                Algumas das principais características que permeia os sistemas embarcados são que estes <strong>interagem com o ambiente </strong>que os cerca
+                            </p>
+                            <ul className="listaCaract">
+                                <li>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="#000000" className="bi bi-phone-fill icone" viewBox="0 0 16 16">
+                                        <path d="M3 2a2 2 0 0 1 2-2h6a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V2zm6 11a1 1 0 1 0-2 0 1 1 0 0 0 2 0z"/>
+                                    </svg>
+                                    Geralmente são móveis
+                                </li>
+                                <li>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="#0EFACF" className="bi bi-lightning-charge-fill icone" viewBox="0 0 16 16">
+                                        <path d="M11.251.068a.5.5 0 0 1 .227.58L9.677 6.5H13a.5.5 0 0 1 .364.843l-8 8.5a.5.5 0 0 1-.842-.49L6.323 9.5H3a.5.5 0 0 1-.364-.843l8-8.5a.5.5 0 0 1 .615-.09z"/>
+                                    </svg>
+                                    Consumo energético deste deve ser mínimo e bem balanceado
+                                </li>
+                                <li>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="#FB8C0A" className="bi bi-memory icone" viewBox="0 0 16 16">
+                                        <path d="M1 3a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h4.586a1 1 0 0 0 .707-.293l.353-.353a.5.5 0 0 1 .708 0l.353.353a1 1 0 0 0 .707.293H15a1 1 0 0 0 1-1V4a1 1 0 0 0-1-1H1Zm.5 1h3a.5.5 0 0 1 .5.5v4a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-4a.5.5 0 0 1 .5-.5Zm5 0h3a.5.5 0 0 1 .5.5v4a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-4a.5.5 0 0 1 .5-.5Zm4.5.5a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 .5.5v4a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-4ZM2 10v2H1v-2h1Zm2 0v2H3v-2h1Zm2 0v2H5v-2h1Zm3 0v2H8v-2h1Zm2 0v2h-1v-2h1Zm2 0v2h-1v-2h1Zm2 0v2h-1v-2h1Z"/>
+                                    </svg>
+                                    A disponibilidade de memória é muito limitada então um código fonte bem otimizado e enxuto é necessário
+                                </li>
+                                <li>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="#FB0A27" className="bi bi-cpu-fill icone" viewBox="0 0 16 16">
+                                        <path d="M6.5 6a.5.5 0 0 0-.5.5v3a.5.5 0 0 0 .5.5h3a.5.5 0 0 0 .5-.5v-3a.5.5 0 0 0-.5-.5h-3z"/>
+                                        <path d="M5.5.5a.5.5 0 0 0-1 0V2A2.5 2.5 0 0 0 2 4.5H.5a.5.5 0 0 0 0 1H2v1H.5a.5.5 0 0 0 0 1H2v1H.5a.5.5 0 0 0 0 1H2v1H.5a.5.5 0 0 0 0 1H2A2.5 2.5 0 0 0 4.5 14v1.5a.5.5 0 0 0 1 0V14h1v1.5a.5.5 0 0 0 1 0V14h1v1.5a.5.5 0 0 0 1 0V14h1v1.5a.5.5 0 0 0 1 0V14a2.5 2.5 0 0 0 2.5-2.5h1.5a.5.5 0 0 0 0-1H14v-1h1.5a.5.5 0 0 0 0-1H14v-1h1.5a.5.5 0 0 0 0-1H14v-1h1.5a.5.5 0 0 0 0-1H14A2.5 2.5 0 0 0 11.5 2V.5a.5.5 0 0 0-1 0V2h-1V.5a.5.5 0 0 0-1 0V2h-1V.5a.5.5 0 0 0-1 0V2h-1V.5zm1 4.5h3A1.5 1.5 0 0 1 11 6.5v3A1.5 1.5 0 0 1 9.5 11h-3A1.5 1.5 0 0 1 5 9.5v-3A1.5 1.5 0 0 1 6.5 5z"/>
+                                    </svg>
+                                    Recursos de hardware são bem limitados com a frequência do clock e tensão sendo as menores possíveis
+                                </li>
+                                <li>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="#072FFF" className="bi bi-award-fill icone" viewBox="0 0 16 16">
+                                        <path d="m8 0 1.669.864 1.858.282.842 1.68 1.337 1.32L13.4 6l.306 1.854-1.337 1.32-.842 1.68-1.858.282L8 12l-1.669-.864-1.858-.282-.842-1.68-1.337-1.32L2.6 6l-.306-1.854 1.337-1.32.842-1.68L6.331.864 8 0z"/>
+                                        <path d="M4 11.794V16l4-1 4 1v-4.206l-2.018.306L8 13.126 6.018 12.1 4 11.794z"/>
+                                    </svg>
+                                    O uso eficiente do hardware pode vir a reduzir o custo final do sistema embarcado
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </Modal>
+        </>
     );
 }
 export default Introducao;
